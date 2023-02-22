1fe258ccf2aff900ee28f26f0b8f63b31e996637
partabon <126032326+partabon@users.noreply.github.com>
2023-02-21 20:20:25 -0600

    Add files via upload

---
+++ hola mundo.py
@@ -0,0 +1,22 @@
+# hola mundo
+print("hola chamaco")
+
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)             # suma(+)
+print(3 - 1)             # resta(-)
+print(2 * 3)             # multiplicación(*)
+print(3 / 2)             # división(/)
+print(3 ** 2)            # potencia(**)
+print(3 % 2)             # módulo o residuo(%)
+print(3 // 2)            # División entera(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
