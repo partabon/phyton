4b373a15219469897982128466e9f9861441d37c
partabon <126032326+partabon@users.noreply.github.com>
2023-03-20 21:23:16 -0600

    Add files via upload

---
+++ dia10/99_bottles.py
@@ -0,0 +1,5 @@
+
+for i in range(99):
+    print(f'{99-i} bottles of beer on the wall')
+    print(f'{99-i} bottles of beer')
+    print('Take one down, pass it around')
---
+++ dia10/detention.py
@@ -0,0 +1,2 @@
+for i in range(100):
+    print(i+1,".- I will not use Snapchat in class")
